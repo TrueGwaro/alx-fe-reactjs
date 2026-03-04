03652f8a6e69ac68f2c012875a0626ff1a93ed7a
Trusilla <lavinatrue@gmail.com>
2026-03-04 23:58:06 +0300

    Initial React project setup for GitHub User Search

---
+++ src/services/githubService.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const GITHUB_API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
+
+export const searchUsers = async (query) => {
+  const response = await axios.get(`https://api.github.com/search/users?q=${query}`, {
+    headers: {
+      Authorization: `token ${GITHUB_API_KEY}`,
+    },
+  });
+  return response.data.items;
+};
