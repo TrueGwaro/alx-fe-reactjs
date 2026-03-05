52839f02e11518debce43f80860fac06084a5537
Trusilla <lavinatrue@gmail.com>
2026-03-05 21:16:47 +0300

    Setup GitHub User Search React project

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,39 +1,35 @@
-import { useState } from "react";
-import { searchUsers } from "./services/githubService";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [username, setUsername] = useState("");
-  const [users, setUsers] = useState([]);
-
-  const handleSearch = async () => {
-    const results = await searchUsers(username);
-    setUsers(results);
-  };
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1>GitHub User Search</h1>
-
-      <input
-        type="text"
-        placeholder="Search GitHub username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-
-      <button onClick={handleSearch}>Search</button>
-
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <a href={user.html_url} target="_blank">
-              {user.login}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
