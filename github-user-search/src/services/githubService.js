c91ffec72c3d2bf51ddef17681102f8a86053e84
Trusilla <lavinatrue@gmail.com>
2026-03-09 18:26:05 +0300

    Ready for deploymenta to Vercel

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,17 +1,11 @@
 // src/services/githubService.js
 import axios from "axios";
 
-// Rename to exactly what ALX expects
 export const fetchUserData = async (username, location, minRepos, page = 1) => {
   let query = username ? `${username}` : "";
 
-  if (location) {
-    query += `+location:${location}`;
-  }
-
-  if (minRepos) {
-    query += `+repos:>=${minRepos}`;
-  }
+  if (location) query += `+location:${location}`;
+  if (minRepos) query += `+repos:>=${minRepos}`;
 
   const response = await axios.get(
     `https://api.github.com/search/users?q=${query}&page=${page}&per_page=10`
